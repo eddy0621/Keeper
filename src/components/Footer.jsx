751068d036459f63090d4a7ca08ca25821f60331
Aditya <hit19ece.aditya@gmail.com>
2022-03-26 15:21:02 +0530

    Initail commit created UI for the app v01.00

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+let date = new Date().getFullYear();
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright © {date}</p>
+    </footer>
+  );
+}
+
+export default Footer;
